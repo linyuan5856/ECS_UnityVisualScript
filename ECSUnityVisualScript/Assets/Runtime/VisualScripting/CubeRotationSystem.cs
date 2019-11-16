b4e6fd69260b61b2ff769e7ccaf513af0e117ec3
linyuan <linyuan5856139@163.com>
2019-11-16 23:36:30 +0800

    update

--- ECSUnityVisualScript/Assets/Runtime/VisualScripting/CubeRotationSystem.cs
+++ ECSUnityVisualScript/Assets/Runtime/VisualScripting/CubeRotationSystem.cs
@@ -13,15 +13,16 @@ public class CubeRotationSystem : ComponentSystem
     private Unity.Entities.EntityQuery Cube_Query;
     protected override void OnCreate()
     {
-        Cube_Query = GetEntityQuery(ComponentType.ReadWrite<Unity.Transforms.Rotation>(), ComponentType.ReadOnly<CubeRotationComponent>());
+        Cube_Query = GetEntityQuery(ComponentType.ReadWrite<Unity.Transforms.Rotation>(), ComponentType.ReadWrite<Unity.Transforms.Translation>(), ComponentType.ReadOnly<CubeRotationComponent>());
     }
 
     protected override void OnUpdate()
     {
         {
-            Entities.With(Cube_Query).ForEach((Unity.Entities.Entity Cube_QueryEntity, ref Unity.Transforms.Rotation Cube_QueryRotation, ref CubeRotationComponent Cube_QueryCubeRotationComponent) =>
+            Entities.With(Cube_Query).ForEach((Unity.Entities.Entity Cube_QueryEntity, ref Unity.Transforms.Rotation Cube_QueryRotation, ref CubeRotationComponent Cube_QueryCubeRotationComponent, ref Unity.Transforms.Translation Cube_QueryTranslation) =>
             {
                 Cube_QueryRotation.Value = math.mul(Cube_QueryRotation.Value, quaternion.AxisAngle(math.normalize(new float3(0F, 1F, 0F)), math.mul(Time.deltaTime, Cube_QueryCubeRotationComponent.speed)));
+                Cube_QueryTranslation.Value = new float3(0F, 0F, 0F);
             }
 
             );
